fb94e7f5a243b096fd236bb07eef67d2cc806c4c
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2025-10-20 12:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (12.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -16,7 +16,19 @@ public:
         int j=n-1;
         //m rows
         //n cols
-        vector<vector<int>>DP(m,vector<int>(n,-1));
-        return solve(i,j,m,n,DP);
+        vector<vector<int>>DP(m,vector<int>(n));
+        DP[0][0]=1;
+        for(int i=0;i<n;i++)DP[0][i]=1;
+        for(int i=0;i<m;i++)DP[i][0]=1;
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int up=DP[i-1][j];
+                int left=DP[i][j-1];
+                DP[i][j]=up+left;
+            }
+        }
+        return DP[m-1][n-1];
+
     }
 };
