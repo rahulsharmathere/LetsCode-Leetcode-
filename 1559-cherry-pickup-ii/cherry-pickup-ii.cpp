b74a5ad9aefce080fce4fc21168eb8e16372e70f
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2025-10-22 17:05:53 +0530

    Time: 94 ms (8.21%) | Memory: 20.9 MB (53.23%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    int solve(int i,int j1,int j2,int n,int m,vector<vector<vector<int>>>&DP,vector<vector<int>>&grid){
-        if(j1<0 || j2<0 || j1>=m || j2>=m)return -1e8;
-        if(i==n-1){
-            if(j1!=j2)
-                return grid[i][j1]+grid[i][j2];
-            return grid[i][j1];
-        }
-        if(DP[i][j1][j2]!=-1)return DP[i][j1][j2];
-        //total 9 combos
-        int dra[9]={-1,-1,-1,0,0,0,1,1,1};
-        int drb[9]={-1,0,1,-1,0,1,-1,0,1};
-        int option=-1e8;
-        for(int k=0;k<9;k++){
-            if(j1==j2)
-                option=max(option,grid[i][j1]+solve(i+1,j1+dra[k],j2+drb[k],n,m,DP,grid));
-            else
-                option=max(option,grid[i][j1]+grid[i][j2]+solve(i+1,j1+dra[k],j2+drb[k],n,m,DP,grid));
-
-        }
-        return  DP[i][j1][j2] = option;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         //one robo 0,0
         //another robo 0,m-1
-        vector<vector<vector<int>>>DP(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return solve(0,0,m-1,n,m,DP,grid);
+        vector<vector<vector<int>>>DP(n,vector<vector<int>>(m,vector<int>(m,0)));
+
+        for(int j=0;j<m;j++){
+            for(int k=0;k<m;k++){
+                if(j==k){
+                    DP[n-1][j][k]=grid[n-1][k];
+                }else
+                    DP[n-1][j][k]=grid[n-1][k]+grid[n-1][j];
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int dra[9]={-1,-1,-1,0,0,0,1,1,1};
+                    int drb[9]={-1,0,1,-1,0,1,-1,0,1};
+                    int option=-1e8;
+
+                    for(int k=0;k<9;k++){
+                        int nj1=j1+dra[k];
+                        int nj2=j2+drb[k];
+                        if(nj1<0 || nj2<0 || nj1>=m || nj2>=m)continue;
+                        if(j1==j2)
+                            option=max(option,grid[i][j1]+DP[i+1][nj1][nj2]);
+                        else
+                            option=max(option,grid[i][j1]+grid[i][j2]+DP[i+1][nj1][nj2]);
+                    }
+
+                    DP[i][j1][j2]=option;
+
+                }
+            
+            }
+        }
+        return DP[0][0][m-1];
+
+
     }
 };
