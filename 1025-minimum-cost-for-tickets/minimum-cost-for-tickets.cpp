70af0266fe02de336fc6a805142ff3ea6fc98734
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2025-10-28 09:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (85.17%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&days,vector<int>&costs,int i,vector<int>&DP){
-        //invalid
-        if(i>=n)return 0;
-        if(DP[i]!=-1)return DP[i];
-        //3options
-        //1 day ticket
-        int opt1=costs[0]+solve(n,days,costs,i+1,DP);
-        //7 day ticket
-        int j=i;
-        while(j<n && days[j]<days[i]+7){
-            j++;
-        }
-        int opt2=costs[1]+solve(n,days,costs,j,DP);
-        //30 day ticket
-        j=i;
-        while(j<n && days[j]<days[i]+30){
-            j++;
-        }
-        int opt3=costs[2]+solve(n,days,costs,j,DP);
-        return DP[i]= min(opt1,min(opt2,opt3));
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        // costs [1day, 7day, 30day]
-        int n=days.size();
-        vector<int>DP(n,-1);
-        int ans=solve(n,days,costs,0,DP);
+        int ans=0;
+        queue<pair<int,int>>month;
+        queue<pair<int,int>>week;
+
+        for(int day:days){
+            //
+            while(!month.empty() && month.front().first + 30 <= day)
+                month.pop();
+            while(!week.empty() && week.front().first + 7 <= day)
+                week.pop();
+
+            week.push({day,ans+costs[1]});
+            month.push({day,ans+costs[2]});
+
+            ans=min(ans+costs[0],min(week.front().second,month.front().second));
+        }
         return ans;
     }
 };
