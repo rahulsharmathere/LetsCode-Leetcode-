db03cd7f2b7f95f98aa104ac3f2e272f3476ebf4
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2025-12-23 08:02:04 +0530

    Time: 6 ms (64.86%) | Memory: 11.9 MB (58.39%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int solve(int i,int j,int m,int n,int maxMove,vector<vector<vector<int>>>&dp){
         static const int MOD=1e9+7;
-        if(maxMove<0)return 0;
         if(i<0 || j<0 || i>=m || j>=n){
             return 1;
         }
+        if(maxMove<=0)return 0;
         if(dp[i][j][maxMove]!=-1)return dp[i][j][maxMove];
         //4 ways:
         long long up=solve(i-1,j,m,n,maxMove-1,dp);
@@ -21,5 +21,9 @@ public:
         vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(maxMove+1,-1)));
         ans=solve(i,j,m,n,maxMove,dp);
         return ans;
+
+
+       
+
     }
 };
