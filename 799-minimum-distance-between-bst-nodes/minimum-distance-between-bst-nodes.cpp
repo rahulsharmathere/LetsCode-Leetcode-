bd729b00820a885aab3289d23cdeb604855b7a4f
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-01-19 20:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (56.12%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -11,21 +11,21 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*root,vector<int>&nums){
+    void inorder(TreeNode*root,TreeNode*&prev,int &mini){
         if(!root)return ;
-        inorder(root->left,nums);
-        nums.push_back(root->val);
-        inorder(root->right,nums);
+        inorder(root->left,prev,mini);
+        if(prev){
+            mini=min(mini,abs(prev->val-root->val));
+        }
+        prev=root;
+        inorder(root->right,prev,mini);
     }
     int minDiffInBST(TreeNode* root) {
         // minimum difference between any 2 nodes
         // it will be between 2 consecutive elements
-        vector<int>nums;
-        inorder(root,nums);
         int mini=INT_MAX;
-        for(int i=1;i<nums.size();i++){
-            mini=min(mini,abs(nums[i]-nums[i-1]));
-        }
+        TreeNode*prev=NULL;
+        inorder(root,prev,mini);
         return mini;
     }
 };
