3e5afcc05a25bdfc68ea99f207a9ae874e1bd4fb
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-03-16 12:05:53 +0530

    Time: 198 ms (31.72%) | Memory: 61.8 MB (36.55%) - LeetSync

---
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        set<int, greater<int>>pq; //priority queue
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                pq.insert(grid[i][j]);
+                for(int k=1; ;k++){
+                    if(i-k<0 || i+k>=n || j-k<0 || j+k>=m)break;
+                    int sum=0;
+                    int x=i-k;
+                    int y=j;
+                    for(int t=0;t<k;t++){
+                        sum+=grid[x+t][y+t];
+                    }
+                    for(int t=0;t<k;t++){
+                        sum+=grid[i+t][j+k-t];
+                    }
+                    for(int t=0;t<k;t++){
+                        sum+=grid[i+k-t][j-t];
+                    }
+                    for(int t=0;t<k;t++){
+                        sum+=grid[i-t][j-k+t];
+                    }
+
+                    pq.insert(sum);
+                }
+            }
+        }
+        vector<int> ans;
+
+        for(auto x:pq){
+            ans.push_back(x);
+            if(ans.size()==3) break;
+        }
+
+        return ans;
+    }
+};
