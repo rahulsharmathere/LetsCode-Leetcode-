2974df5a89c0b816f40ed02e3f18d69582ae18c3
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-03-18 19:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (59.08%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n=s.size();
+        vector<int>freq(26,0);
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+        }
+        vector<int>vis(26,0);
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            char c=s[i];
+            freq[c-'a']--;
+        if(vis[c-'a']==1)continue;
+            if(st.empty()){
+                st.push(c);
+                vis[c-'a']=1;
+                continue;
+            }
+            char t=st.top();
+            if(c>t && vis[c-'a']==0){
+                st.push(c);
+                vis[c-'a']=1;
+            }else if(vis[c-'a']==0){
+                while(!st.empty() && c<st.top() && freq[st.top()-'a']>0){
+                    vis[st.top()-'a']=0;
+                    st.pop();
+                }
+                st.push(c);
+                vis[c-'a']=1;
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
