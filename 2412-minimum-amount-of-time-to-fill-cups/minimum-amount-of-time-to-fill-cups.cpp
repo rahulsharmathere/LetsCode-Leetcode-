05b66e568f3cfb4ca916ba6dc457bc49015e1d2e
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-03-21 21:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (1.72%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int fillCups(vector<int>& amount) {
-        int ans = 0;
-        
-        while(amount[0] > 0 || amount[1] > 0 || amount[2] > 0){
-            sort(amount.begin(),amount.end());
-            amount[1]--;
-            amount[2]--;
-            ans++;
+        int ans = 0; 
+        int n=amount.size();
+        priority_queue<int>pq;
+        for(int i=0;i<n;i++){
+            pq.push(amount[i]);
         }
-
+        while(!pq.empty()){
+            if(pq.top()<=0)break;
+            ans++;
+            int curr1=pq.top();
+            curr1--;
+            pq.pop();
+            int curr2=0;
+            if(!pq.empty()){
+                curr2=pq.top();
+                curr2--;
+                pq.pop();
+            }
+            if(curr1)pq.push(curr1);
+            if(curr2)pq.push(curr2);
+        }
+        
         return ans;
     }
 };
