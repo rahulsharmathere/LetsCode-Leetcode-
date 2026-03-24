06482812f48f21d090c37ef4cf66196d8a27cd6f
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-03-24 21:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,bool taken,vector<vector<int>>&dp){
-        if(i==nums.size())return 0;
-        if(dp[i][taken]!=-1)return dp[i][taken];
-        //take
-        int ans1=0;
-        if(!taken)
-            ans1=nums[i]+solve(nums,i+1,true,dp);
-        // leave
-        int ans2=solve(nums,i+1,false,dp);
-
-        return dp[i][taken]=max(ans1,ans2);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
-        bool flag=false;
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return solve(nums,i,flag,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        //base
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                //take
+                int ans1=0;
+                if(!j)
+                    ans1=nums[i]+dp[i+1][1];
+                // leave
+                int ans2=dp[i+1][0];
+
+                dp[i][j]=max(ans1,ans2);
+            }
+        }
+        return dp[0][0];
+
     }
 };
