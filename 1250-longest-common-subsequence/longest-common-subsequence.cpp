d82eaf884341487a3bf3d61ffc77c275cb1d3c12
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-03-30 17:51:17 +0530

    Time: 26 ms (66.04%) | Memory: 27.4 MB (67.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,27 +1,32 @@
 class Solution {
   public:
-    int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-        int n=s1.size();
-        int m=s2.size();
-        if(i==n-1 && m==n-1){
-            return (s1[i]==s2[j]);
-        }
-        if(i>=n || j>=m)return 0;
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        //match
-        if(s1[i]==s2[j])return dp[i][j]=1+solve(s1,s2,i+1,j+1,dp);
-        //not match
-        return dp[i][j]= 0+max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
-    }
+    
     int longestCommonSubsequence(string& s1, string& s2) {
         // your code here
         int n=s1.size();
         int m=s2.size();
-        int i=0;
-        int j=0;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(s1,s2,i,j,dp);
+        // int i=0;
+        // int j=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        //base
+        for(int i=0;i<=n;i++){
+            dp[i][m]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[n][i]=0;
+        }
+        dp[n-1][m-1]=s1[n-1]==s2[m-1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1)continue;
+                //match
+                if(s1[i]==s2[j]) dp[i][j]=1+dp[i+1][j+1];
+                //not match
+                else dp[i][j]= 0+max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+
     }
 };
 
