c2b7e4031dc33532c49ceb482366ad00d065b2fb
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-06-19 08:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (45.47%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        uint32_t res = 0;
+        int res = 0;
         for(int i=0 ; i<32 ; i++){
             if(n&1){
                 res=res|(1<<(31-i));
