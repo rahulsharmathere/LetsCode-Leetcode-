16e3c420370f3b4735066db2f83e901e82d7118d
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-06-23 17:06:33 +0530

    Time: 21 ms (36.63%) | Memory: 65 MB (90.80%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -6,18 +6,22 @@ public:
         //only one repeated number in nums
         int left=1;
         int right=n-1;
+        int ans=-1;
         while(left<right){
             int mid=left-(left-right)/2;
             int cnt=0;
             for(int num:nums){
                 if(num<=mid){
-                    if(num<=mid)cnt++;
+                    cnt++;
                 }
             }
             if(cnt>mid)
                 right=mid;
-            else 
+            else{
+                ans=mid;
                 left=mid+1;
+            } 
+                
         }
         return left;
     }
