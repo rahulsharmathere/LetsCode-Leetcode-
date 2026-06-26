2bf0d87513af82e78da2b17de84fef744ef2e3d1
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-06-26 07:49:49 +0530

    Time: 10 ms (19.26%) | Memory: 14.9 MB (96.12%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,32 +17,54 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        //without using map 
         if(head==NULL)return NULL;
-        Node* temp=head;
-        while(temp!=NULL){
-            Node* newNode = new Node(temp->val);
-            Node* forw=temp->next;
-            temp->next=newNode;
-            newNode->next=forw;
-            temp=temp->next->next;
+        //without using map
+        Node* t=head;
+        Node* t2=new Node(head->val);
+        Node* head2=t2;
+        t=t->next;
+        //step 1
+        while(t!=NULL){
+            t2->next=new Node(t->val);
+            t2=t2->next;
+            t=t->next;
         }
-        temp=head;
-        while(temp!=NULL){
-            if (temp->random != NULL)
-                temp->next->random = temp->random->next;
-            temp=temp->next->next;
+        //step 2 
+        t=head;
+        Node* tnext=NULL;
+        t2=head2;
+        Node* t2next=NULL;
+        while(t!=NULL){
+            tnext=t->next;
+            t2next=t2->next;
+            t->next=t2;
+            t2->next=tnext;
+            t=tnext;
+            t2=t2next;
         }
-        Node* dummy = new Node(-1);
-        Node*copyTail=dummy;
-        temp = head;
-        while (temp != NULL) {
-            Node* copyNode = temp->next;
-            temp->next = copyNode->next;  // restore original next
-            copyTail->next = copyNode;    // build copied list
-            copyTail = copyNode;
-            temp = temp->next;
+        //step3
+        t2=head2;
+        t=head;
+        while(t!=NULL){
+            if(t->random)t2->random=t->random->next;
+            else t2->random=NULL;
+            t=t->next->next;
+            if(t2->next)t2=t2->next->next;
         }
-        return dummy->next;
+        //reseting next pointers
+        t=head;
+        t2=head2;
+        while(t!=NULL){
+            t->next=t2->next;
+            if(t->next)t2->next=t->next->next;
+            
+            t=t->next;
+            t2=t2->next;
+        }
+
+
+        return head2;
+        
+
     }
 };
