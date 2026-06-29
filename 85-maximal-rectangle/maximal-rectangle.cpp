4f60172753e93607576b6de1577179d5491b65b2
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-06-29 10:13:35 +0530

    Time: 15 ms (26.21%) | Memory: 20.9 MB (39.63%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,57 +1,53 @@
 class Solution {
 public:
-    //khud nahi kiya , ;)
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
-        st.push(-1);
-
-        vector<int> prevSmaller(n), nextSmaller(n);
-
-        for (int i = 0; i < n; i++) {
-            while (st.top() != -1 && heights[st.top()] >= heights[i])
-                st.pop();
-            prevSmaller[i] = st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) st.pop();
-        st.push(-1);
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.top() != -1 && heights[st.top()] >= heights[i])
-                st.pop();
-            nextSmaller[i] = (st.top() == -1) ? n : st.top();
-            st.push(i);
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int height = heights[i];
-            int width = nextSmaller[i] - prevSmaller[i] - 1;
-            maxArea = max(maxArea, height * width);
-        }
-
-        return maxArea;
-    }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-
-        vector<int> histogram(cols, 0);
-        int answer = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == '1')
-                    histogram[j]++;
-                else
-                    histogram[j] = 0;
+        if(matrix.empty()) return 0;
+        int s=matrix.size();
+        int m=matrix[0].size();
+        vector<int>heights(m,0);
+        int ans=0;
+        for(int i=0;i<s;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    heights[j]++;
+                }else heights[j]=0;
             }
-            answer = max(answer, largestRectangleArea(histogram));
+            int n=heights.size();
+            //max rectangle = h*((nse)-(pse)+1)
+            stack<int>st;
+            vector<int>pse(n,-1);
+            vector<int>nse(n,-1);
+
+            st.push(n);
+            for(int i=n-1;i>=0;i--){
+                while(st.top()!=n && heights[st.top()] >= heights[i]){
+                    st.pop();
+                }
+                nse[i]=st.top();
+                st.push(i);
+            }
+            while(!st.empty())st.pop();
+            st.push(-1);
+            for(int i=0;i<n;i++){
+                while(st.top()!=-1 && heights[st.top()] >= heights[i]){
+                    st.pop();
+                }
+                pse[i]=st.top();
+                st.push(i);
+            }
+            int maxAr=0;
+            for(int i=0;i<n;i++){
+                int h=heights[i];
+                int l=pse[i];
+                int r=nse[i];
+                int ar=h*(r-l-1);
+
+                maxAr=max(maxAr,ar);
+            }
+            ans=max(maxAr,ans);
+
         }
 
-        return answer;
+        return ans;
     }
 };
