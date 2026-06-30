40c6479765061582841e74c3882f796705d5b1b4
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-06-30 22:22:12 +0530

    Time: 12 ms (93.09%) | Memory: 50.6 MB (53.89%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,65 +1,80 @@
-struct Node{
-    Node*links[26];
-    bool flag=false;
-    bool containsKey(char ch){
-        return (links[ch-'a']!=NULL);
+class Node{
+public:
+    Node*arr[26];
+    bool flag;
+    
+    Node(){
+        for(int i=0;i<26;i++){
+            arr[i]=NULL;
+        }
+        flag=false;
     }
 
-    void put(char ch,Node*node){
-        links[ch-'a']=node;
+    bool checkKey(char ch){
+        if(arr[ch-'a']!=NULL)return true;
+        return false;
     }
 
-    Node* get(char ch){
-        return  links[ch-'a'];
+    void insert(char ch,Node* newNode){
+        arr[ch-'a']=newNode;
+    }
+    
+    Node* getNode(char ch){
+        return arr[ch-'a'];
     }
 
     void setEnd(){
         flag=true;
     }
-
     bool isEnd(){
         return flag;
     }
 };
+
 class Trie {
 public:
     Node* root;
-    Trie(){
-        root=new Node();
-    }
-    //O(n)
-    void insert(string word) {
-        Node* node = root;
-        //traverse
-        for(int i=0;i<word.size();i++){
-            //char not present -> add char
-            if(!node->containsKey(word[i])){
-                node->put(word[i],new Node());
-            }
-            node=node->get(word[i]);
-        }
-        node->setEnd();
+    Trie() {
+        root=new Node;
     }
     
-    //O(len)
-    bool search(string word) {
-       Node*node=root;
-        for(int i=0;i<word.size();i++){
-            if(!node->containsKey(word[i])){
-                return false;
-            }
-            node=node->get(word[i]);
-        } 
-       return node->isEnd();
-    }
-    //O(len)
-    bool startsWith(string prefix) {
+    void insert(string word) {
+        int n=word.size();
         Node* node=root;
-        for(int i=0;i<prefix.size();i++){
-            if(!node->containsKey(prefix[i])){
+        for(int i=0;i<n;i++){
+            char ch=word[i];
+            if(!node->checkKey(ch)){
+                Node* newNode=new Node;
+                node->insert(ch,newNode);
+            }
+            node=node->getNode(ch);
+        }
+        node->setEnd();
+
+    }
+    
+    bool search(string word) {
+        int n=word.size();
+        Node* node=root;
+        for(int i=0;i<n;i++){
+            char ch=word[i];
+            if(!node->checkKey(ch)){
                 return false;
             }
-            node=node->get(prefix[i]);
+            node=node->getNode(ch);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string word) {
+        int n=word.size();
+        Node* node=root;
+        for(int i=0;i<n;i++){
+            char ch=word[i];
+            if(!node->checkKey(ch)){
+                return false;
+            }
+            node=node->getNode(ch);
         }
         return true;
     }
