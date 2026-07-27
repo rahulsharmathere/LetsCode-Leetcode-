ce0b16739af3cc967e196b80d4ddfd58a62540a8
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-07-27 20:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (63.59%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -10,7 +10,7 @@ public:
             if(nums[r]==0){
                 zeros++;
             }
-            while (zeros > k) {
+            if (zeros > k) {
                 if (nums[l] == 0)
                     zeros--;
                 l++;
