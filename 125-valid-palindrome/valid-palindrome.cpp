ad9bb13e41a90ec236d721234b2ec726c5c5f1a4
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-07-28 21:21:18 +0530

    Time: 6 ms (6.18%) | Memory: 10.3 MB (24.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0, j = s.size() - 1;
+        int k=0;
+        vector<char>s2(s.size(),' ');
 
-        while (i < j) {
-            while (i < j && !isalnum(s[i])) i++;
-            while (i < j && !isalnum(s[j])) j--;
-
-            if (tolower(s[i]) != tolower(s[j]))
+        while(k<s.size()){
+            if(s[k]>=65 && s[k]<=90){
+                s2[k]=s[k]+32;
+            }
+            else if(s[k]>=97 && s[k]<=122){
+                s2[k]=s[k];
+            }
+            else if(s[k]<=57 && s[k]>= 48){
+                s2[k]=s[k];
+            }else{
+                s2[k]=' ';
+            }
+            k++;
+        }
+        int i=0;
+        int j=s2.size()-1;
+        while(i<j){
+            if(s2[i]==' '){
+                i++;
+                continue;
+            }
+            if(s2[j]==' '){
+                j--;
+                continue;
+            }
+            if(s2[i]!=s2[j]){
                 return false;
-
+            }
             i++;
             j--;
         }
-
         return true;
     }
 };
