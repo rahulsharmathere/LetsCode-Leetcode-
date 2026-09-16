38c0a37d77ae497af1f906b3950e29b9ac650217
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-09-16 08:16:36 +0530

    Time: 71 ms (81.17%) | Memory: 56.2 MB (57.53%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int k){
-        //calculates no of subarays with no of diff integers <= k (atmost k)
+    int solve(vector<int>&nums,int k){
         int n=nums.size();
         int r=0;
         int l=0;
-        map<int,int>mp;
         int cnt=0;
+        unordered_map<int,int>mp;
+
         while(r<n){
             mp[nums[r]]++;
             while(mp.size()>k){
                 mp[nums[l]]--;
-                if(mp[nums[l]]==0){
-                    mp.erase(nums[l]);
-                }
+                if(mp[nums[l]]==0)mp.erase(nums[l]);
                 l++;
             }
-
-            if(mp.size()<=k){
-                cnt+=r-l+1;
-            }
+            cnt+=r-l+1;
             r++;
         }
         return cnt;
-
     }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        //good ab samajh gya 
-        //calculates no of subarays with no of diff integers = k
-        int ans1 = solve(nums,k-1);
-        int ans2 = solve(nums,k);
-        int ans = ans2-ans1;
-        return ans;
+        int n=nums.size();
+        return solve(nums,k)-solve(nums,k-1);
     }
 };
