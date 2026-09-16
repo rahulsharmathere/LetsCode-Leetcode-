13a37aeccd470e81d07e50fabb5c7261931dd1ca
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-09-16 10:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.84%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
+    int n;
+    vector<vector<int>> dp;
 
-        for (char c : s) {
-            if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
-            }
-            if (leftMax < 0) return false;
-            if (leftMin < 0) leftMin = 0;
+    bool solve(string &s, int index, int cnt) {
+        if (cnt < 0) return false;
+
+        if (index == n) {
+            return cnt == 0;
         }
-        
-        return leftMin == 0;
+
+        if (dp[index][cnt] != -1) {
+            return dp[index][cnt];
+        }
+
+        bool ans;
+
+        if (s[index] == '(') {
+            ans = solve(s, index + 1, cnt + 1);
+        }
+        else if (s[index] == ')') {
+            ans = solve(s, index + 1, cnt - 1);
+        }
+        else {
+            ans =
+                solve(s, index + 1, cnt + 1) ||
+                solve(s, index + 1, cnt - 1) ||
+                solve(s, index + 1, cnt);
+        }
+
+        return dp[index][cnt] = ans;
+    }
+
+    bool checkValidString(string s) {
+        n = s.size();
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(s, 0, 0);
     }
 };
