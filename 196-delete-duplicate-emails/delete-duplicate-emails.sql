57ee7fe6e217bc3e74996e49fad43a7b67379d3e
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-09-22 20:33:44 +0530

    Time: 552 ms (98.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT p2.id
+        FROM Person p1, Person p2
+        WHERE p1.email = p2.email
+        AND p1.id < p2.id
+    ) AS duplicates
+);
