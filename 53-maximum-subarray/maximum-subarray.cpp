c8d0cb9aabe908e26eba2d371312f8871073c93c
rahulsharmathere <155520144+rahulsharmathere@users.noreply.github.com>
2026-09-22 18:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,9 +5,8 @@ public:
         int sum=0;
         int maxSum=INT_MIN;
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+            sum=max(sum+nums[i],nums[i]);
             maxSum=max(maxSum,sum);
-            if(sum<0)sum=0;
         }
         return maxSum;
     }
